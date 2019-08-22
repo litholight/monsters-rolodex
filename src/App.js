d88a37329c973210e7a750b7a0dc534cb93851ef
Stoneburner Software <sean@stoneburnersoftware.com>
2019-08-21 22:56:43 -0400

    monster hash and Card component

--- src/App.js
+++ src/App.js
@@ -6,23 +6,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monster: []
+      monsters: []
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ monster: users }));
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
     return (
       <div className="App">
-        <CardList name="Sean">
-          {this.state.monster.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-          ))}
-        </CardList>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
