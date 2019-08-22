4ad9768d6f0ca8279386e04539245527a4022477
Stoneburner Software <sean@stoneburnersoftware.com>
2019-08-21 22:32:30 -0400

    exploring React classes and state

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      monster: [
-      { 
-        name: 'Frankenstein',
-        id: 'dsflkj2'
-      },
-      { 
-        name: 'Dracula',
-        id: 'alskd4'
-      },
-      { 
-        name: 'Zombie',
-        id: 'akfbk4'
-      }
-      
-    ]}
+      monster: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monster: users }));
   }
   render() {
     return (
       <div className="App">
-        {
-          this.state.monster.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList name="Sean">
+          {this.state.monster.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
-    ); 
+    );
   }
 }
 
