aa26f8bb5b1ba69aa3563e6ffff696e3b773d3a8
Stoneburner Software <sean@stoneburnersoftware.com>
2019-08-22 21:55:52 -0400

    Updates

---
+++ precache-manifest.4473de99c946288d2297d475bf7138e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abf6359a766c4576e509f225c521676",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "816f5f20aaec1e0438ca",
+    "url": "/monsters-rolodex/static/css/main.1266b439.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monsters-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "816f5f20aaec1e0438ca",
+    "url": "/monsters-rolodex/static/js/main.a5c900c7.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
